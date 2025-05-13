dbe6c88d84be84bbd9bdb7550b0415ac064ed524
lum8rjack <4932619+lum8rjack@users.noreply.github.com>
2025-05-13 00:09:35 -0500

    added function to quickly check if user is able to edit or not

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -3,3 +3,7 @@ import pocketbase from '../pocketbase'
 export const isAuthenticated = () => {
   return pocketbase.authStore.isValid
 } 
+
+export const canEdit = () => {
+  return pocketbase.authStore.isValid && pocketbase.authStore.record.role !== 'viewer'
+}
